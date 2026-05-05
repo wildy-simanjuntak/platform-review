87666f858ea0380a1d4d5cb885604b293150a0be
wildy-simanjuntak <simanjuntakwildy13@gmail.com>
2026-05-05 20:17:47 +0700

    deleted

--- #web/app/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="bg-gray-100">
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-
-    <UNotifications />
-  </div>
-</template>
--- #web/app/composables/useSocket.js
+++
@@ -1,9 +0,0 @@
-import { io } from 'socket.io-client';
-
-export const useSocket = () => {
-  const { data } = useAuth();
-  const config = useRuntimeConfig();
-  const socket = io(`${config.public.apiUrl}`)
-
-  return { socket };
-};
